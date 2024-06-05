a5fbd26aa8d356382ce6c7ede3249d0c89795588
ChaoYiChen <a951159aca@gmail.com>
2024-06-05 00:40:17 -0700

    test script setup v2

--- test/test.py
+++ test/test.py
@@ -1,12 +1,26 @@
+import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-options = Options()
 
-options.headless = True
+class PythonOrgSearch(unittest.TestCase):
 
-driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
+    def setUp(self):
+        self.driver = webdriver.Firefox()
 
-driver.get("https://google.com/")
-print(driver.title)
-driver.quit()
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element(By.NAME, "q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        self.assertNotIn("No results found.", driver.page_source)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
